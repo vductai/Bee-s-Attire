3e66775bbd721b5e7b7dfaf9c46b9b8ef03e4c84
vductai <vuhech@gmail.com>
2024-09-19 18:51:34 +0700

    policy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class UserPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+}
