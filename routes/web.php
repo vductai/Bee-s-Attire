fb3cb9e74a3fb57bf4354de447f899d717180782
vductai <vuhech@gmail.com>
2024-09-27 20:56:26 +0700

    route prefix

--- routes/web.php
+++ routes/web.php
@@ -13,56 +13,60 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.dashboard');
+Route::prefix('admin')->group(function (){
+    Route::get('/', function () {
+        return view('admin.dashboard');
+    });
+
+    Route::get('/listProduct', function () {
+        return view('admin.product.list-product');
+    })->name('listProduct');
+
+    Route::get('/addProduct', function () {
+        return view('admin.product.add-product');
+    })->name('addProduct');
+
+    Route::get('/editProduct', function () {
+        return view('admin.product.edit-product');
+    })->name('editProduct');
+
+    Route::get('/addVariant', function () {
+        return view('admin.variant.add-variant');
+    })->name('addVariant');
+
+
+
+    Route::get('/listColor', function () {
+        return view('admin.color.list-color');
+    })->name('listColor');
+
+    Route::get('/addColor', function () {
+        return view('admin.color.add-color');
+    })->name('addColor');
+
+    Route::get('/updateColor', function () {
+        return view('admin.color.update-color');
+    })->name('updateColor');
+
+    Route::get('/listSize', function () {
+        return view('admin.size.list-size');
+    })->name('listSize');
+
+    Route::get('/addSize', function () {
+        return view('admin.size.add-size');
+    })->name('addSize');
+
+    Route::get('/updateSize', function () {
+        return view('admin.size.update-size');
+    })->name('updateSize');
+
+
+
+    Route::get('/loginAdmin', function (){
+        return view('admin.auth.login-admin');
+    })->name('loginAdmin');
 });
 
-Route::get('/listProduct', function () {
-    return view('admin.product.list-product');
-})->name('listProduct');
 
-Route::get('/addProduct', function () {
-    return view('admin.product.add-product');
-})->name('addProduct');
-
-Route::get('/editProduct', function () {
-    return view('admin.product.edit-product');
-})->name('editProduct');
-
-Route::get('/addVariant', function () {
-    return view('admin.variant.add-variant');
-})->name('addVariant');
-
-
-
-Route::get('/listColor', function () {
-    return view('admin.color.list-color');
-})->name('listColor');
-
-Route::get('/addColor', function () {
-    return view('admin.color.add-color');
-})->name('addColor');
-
-Route::get('/updateColor', function () {
-    return view('admin.color.update-color');
-})->name('updateColor');
-
-Route::get('/listSize', function () {
-    return view('admin.size.list-size');
-})->name('listSize');
-
-Route::get('/addSize', function () {
-    return view('admin.size.add-size');
-})->name('addSize');
-
-Route::get('/updateSize', function () {
-    return view('admin.size.update-size');
-})->name('updateSize');
-
-
-
-Route::get('/loginAdmin', function (){
-    return view('admin.auth.login-admin');
-})->name('loginAdmin');
 
 /// admin
