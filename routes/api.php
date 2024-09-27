6d777ea41f57696ce504aebf036fc22c45f01994
cuongdz1 <cuongphanvan428@gmail.com>
2024-09-27 22:27:59 +0700

    cường1

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('categories-api', CategoryAPIController::class);
-
-Route::apiResource('vouchers-api', VouchersAPIController::class);
